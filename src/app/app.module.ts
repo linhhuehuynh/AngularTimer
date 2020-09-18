757f331b65453e0fe9634eb2c515a97369bf8cbc
Linh Hue Huynh <linhhuehuynh0512@gmail.com>
2020-09-18 10:26:48 -0400

    first commit timer

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TimerComponent } from './components/timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimerComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
